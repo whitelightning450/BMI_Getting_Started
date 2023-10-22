24393eb5a9a64bac1598b16c79f14c8f1bd32260
maryrosedevega <143856348+maryrosedevega@users.noreply.github.com>
2023-10-22 22:53:56 +0800

    Delete examples directory

--- examples/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Simulate discharge with a linear reservoir model."""
-from ._version import __version__
-from .bmi_linearres import LinearresBmi
-from .linearres import Linearres, solve_linearres
-
-__all__ = ["__version__", "LinearresBmi", "solve_linearres", "Linearres"]
